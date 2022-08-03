f5240deeadfbec67366edf3a678b8965820ddcbe
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-02 20:48:13 -0700

    adjust alt tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import marioImage from './../img/Mario_and_mushroom.png'
 function Header(){
   return (
     <React.Fragment>
-      <img src={marioImage} alt='An image of Mario' />
+      <img src={marioImage} alt='Super Mario' />
       <h1>Mario's Farmer's Market</h1>
       <div><b><em>YAAAAHOOOOOO</em></b></div>
       <hr/>
